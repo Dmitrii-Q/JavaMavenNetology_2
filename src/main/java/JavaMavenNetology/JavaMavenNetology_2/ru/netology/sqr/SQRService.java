78c58dee30e82ce2c673b7f92a55e1019872a894
Dmitrii-Q <120948230+Dmitrii-Q@users.noreply.github.com>
2023-05-26 21:54:32 +0300

    Update SQRService.java

--- src/main/java/JavaMavenNetology/JavaMavenNetology_2/ru/netology/sqr/SQRService.java
+++ src/main/java/JavaMavenNetology/JavaMavenNetology_2/ru/netology/sqr/SQRService.java
@@ -1,5 +1,19 @@
-package JavaMavenNetology.JavaMavenNetology_2.
+package JavaMavenNetology.JavaMavenNetology_2.ru.netology.sqr;
 
 public class SQRService {
 
+    public int calcSquare(int bot, int top) {
+
+        int counter = 0;
+        for (int i = 10; i <= 99; i++) {
+
+            int square = i * i;
+            if (square >= bot) {
+                if (square <= top) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
 }
